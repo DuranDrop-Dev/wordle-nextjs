8506bccd4150c2e0e4cff516881399e8f5cd4261
DuranDrop <gabe.duran82@gmail.com>
2024-03-10 16:45:01 -0600

    UPDATE

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,40 @@ export const metadata: Metadata = {
   title: "Wordle NextJS App",
   description: "Worlde Imitation Written in NextJS",
   manifest: "/manifest.json",
-  icons: "/site.webmanifest",
+  icons: [{
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    url: "https://wordle.durandrop.com/apple-touch-icon.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    url: "https://wordle.durandrop.com/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    url: "https://wordle.durandrop.com/favicon-16x16.png",
+  },
+  {
+    rel: "manifest",
+    url: "https://wordle.durandrop.com/site.webmanifest",
+  },
+  {
+    rel: "mask-icon",
+    url: "https://wordle.durandrop.com/safari-pinned-tab.svg",
+    color: "#030712",
+  },
+  {
+    rel: "shortcut icon",
+    url: "https://wordle.durandrop.com/favicon.ico",
+  }],
 };
 
 export const viewport: Viewport = {
-  themeColor: { media: "(prefers-color-scheme: dark)", color: "#0C0A09" }
+  themeColor: { media: "(prefers-color-scheme: dark)", color: "#030712" }
 }
 
 export default function RootLayout({
